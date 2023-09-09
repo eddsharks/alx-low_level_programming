c1543854e709963b18ffd645e8eaeaa682aa9b53
eddsharks <edmundagyeman7@gmail.com>
2023-09-08 18:45:24 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: Prints single-digit numbers from 0 to 9 followed by a new line.
+ * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int number;
+	int i;
 
-    for (number = 0; number <= 9; number++)
-    {
-        putchar(number + '0');
-        putchar('\n');
-    }
-
-    return (0);
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
