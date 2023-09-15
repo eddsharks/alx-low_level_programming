2962dd0d35c09ed0c2a652f230a379bf6e5e6501
eddsharks <edmundagyeman7@gmail.com>
2023-09-14 17:32:13 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * positive_or_negative - prints if integer is positive, negative, or zero
+ * @i: the integer to check
+ */
+void positive_or_negative(int i)
+{
+    if (i > 0)
+        printf("%d is positive\n", i);
+    else if (i < 0)
+        printf("%d is negative\n", i);
+    else
+        printf("%d is zero\n", i);
+}
 
+/**
+ * main - tests function that prints if integer is positive, negative, or zero
+ * Return: 0
+ */
 int main(void)
 {
-        int i;
+    int i;
 
-        i=0;
-        positive_or_negative(i);
+    i = 98;
+    positive_or_negative(i);
 
-        return (0);
+    return (0);
 }
+
