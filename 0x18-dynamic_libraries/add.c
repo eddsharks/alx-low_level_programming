95fd55dd908b3b5d21e08105f08388e398bbf2ed
eddsharks <edmundagyeman7@gmail.com>
2023-12-11 21:29:05 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - add two integers 
+ * @a: first int 
+ * @b: second int 
+ * Return: addtion
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - modulus of two integers 
+ * @a: first int 
+ * @b: second int 
+ * Return: the modulus
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
